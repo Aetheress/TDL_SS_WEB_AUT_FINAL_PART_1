e3c2a91eb78aa9f361a156b4a846cadf60942b0d
Aetheress <ulvis.blathens@gmail.com>
2022-08-25 09:23:24 +0300

    Part 1, Scenario 2

--- cypress/e2e/SwagLabs.cy.js
+++ cypress/e2e/SwagLabs.cy.js
@@ -12,4 +12,10 @@ describe("Swag Labs", () => {
     LoginPage.login.click();
     LoginPage.error.should("have.text", "Epic sadface: Sorry, this user has been locked out.");
   });
+  it.only("Login with wrong password", () => {
+    LoginPage.username.type("standard_user");
+    LoginPage.password.type("the_sauce_that_everyone_knows");
+    LoginPage.login.click();
+    LoginPage.error.should("have.text", "Epic sadface: Username and password do not match any user in this service");
+  });
 });
