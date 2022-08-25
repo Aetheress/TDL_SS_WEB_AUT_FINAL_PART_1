ec2f960db95cd3e0c6d518ff40079745bd893198
Aetheress <ulvis.blathens@gmail.com>
2022-08-25 09:28:18 +0300

    Part 1, Scenario 3

--- cypress/e2e/SwagLabs.cy.js
+++ cypress/e2e/SwagLabs.cy.js
@@ -5,17 +5,31 @@ describe("Swag Labs", () => {
   beforeEach(() => {
     LoginPage.visit();
   });
-
+  //Scenario 1
   it("Login with locked out user", () => {
     LoginPage.username.type("locked_out_user");
     LoginPage.password.type("secret_sauce");
     LoginPage.login.click();
     LoginPage.error.should("have.text", "Epic sadface: Sorry, this user has been locked out.");
   });
-  it.only("Login with wrong password", () => {
+  //Scenario 2
+  it("Login with wrong password", () => {
     LoginPage.username.type("standard_user");
     LoginPage.password.type("the_sauce_that_everyone_knows");
     LoginPage.login.click();
     LoginPage.error.should("have.text", "Epic sadface: Username and password do not match any user in this service");
   });
+
+
+  context("Actions after logging in", () => {
+    beforeEach(()=> {
+      LoginPage.username.type("standard_user");
+      LoginPage.password.type("secret_sauce");
+      LoginPage.login.click();
+    })
+    //Scenario 3
+    it("Validate item amount", () => {
+      HomePage.items.should("have.length", 6);
+    });
+  })
 });
